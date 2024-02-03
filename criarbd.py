d84352d77857b29e8825303ce9563c5f29661c3a
Bruno Erick <141072611+bericktk@users.noreply.github.com>
2024-02-02 22:58:15 -0300

    Update criarbd.py

--- criarbd.py
+++ criarbd.py
@@ -1,10 +1,10 @@
 # Importando SQLite
 import sqlite3 as lite
 
-# criando conexão
+# Criando conexão
 con = lite.connect('dados.db')
 
-# criando as tabelas
+# Criando as tabelas no SQLite
 with con:
     cur=con.cursor()
     cur.execute("CREATE TABLE socios(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, empresa TEXT, descricao TEXT, marca TEXT, data_de_associacao DATE, valor_mensalidade DECIMAL, serie TEXT, imagem TEXT)")
